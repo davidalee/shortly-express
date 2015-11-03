9bc641fbc7408f67449f9edb821b3eaacb41ae93
David Lee <david7lee@gmail.com>
2015-11-02 19:54:02 -0500

    created user model

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,16 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'user',
+  hasTimestamps: true,
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      var password = bcrypt.hashSync(model.attributes.password);
+      model.set({username: model.attributes.username, password: password});
+    });
+  }
+
 });
 
 module.exports = User;
+
